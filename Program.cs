9f4478209134e6ac633caa2c050c6a3dce8d79c7
Mikhail Blinov <ims_mike@mail.ru>
2018-06-15 20:21:25 +0300

    Убрал комментарии

--- Program.cs
+++ Program.cs
@@ -13,18 +13,29 @@ namespace NjutonMethod
         {
             Func<double, double> func = (x) => Math.Pow(x, 2) - 4;
 
-            List<double> initialValues = ChooseInitialValues(func, 0.51, -10, 10);
+            List<double> initialValues = ChooseInitialValues(func, 0.5, -10, 10);
+
+            List<double> roots = new List<double>();
 
             foreach (double value in initialValues)
             {
 
-                double? root = Calculation(func, 0.01, value, 0.01);
+                double root = Calculation(func, 0.01, value, 0.01);
+
+                roots.Add(root);
 
                 Console.WriteLine(" root это {0}", root);
             }
 
+            IEnumerable<double> newRoots = roots.Union(roots);
+
+            foreach(double root in newRoots)
+                Console.WriteLine(" Новые корни {0}", root);
+
+
             Console.Read();
         }
+
         static double Calculation(Func<double, double> f, double delta, double x0, double epsilum)
         {
             double tmpValue = Double.MaxValue;
@@ -56,16 +67,15 @@ namespace NjutonMethod
             while (leftEdge < rightEdge)
             {
 
-                int firstTestValue = Math.Sign(f(leftEdge));
+               int firstTestValue = Math.Sign(f(leftEdge));
 
                 leftEdge += step;
 
 
-                int secondTestValue = Math.Sign(f(leftEdge));
+               int secondTestValue = Math.Sign(f(leftEdge));
 
-
-                if(firstTestValue != secondTestValue)
-                    values.Add(leftEdge);
+                    if (firstTestValue != secondTestValue)
+                        values.Add(leftEdge);
             }
 
             return values;
